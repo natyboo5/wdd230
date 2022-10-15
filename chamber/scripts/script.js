a17f1c6e81790f5bbce103384a105b32a2d220e3
Natalia <nparedes517@yahoo.com>
2022-10-15 06:59:14 -0400

    more home page content

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -15,3 +15,13 @@ document.querySelector(".date").textContent = new Intl.DateTimeFormat("en-UK", {
 /*Last Modified */
 document.querySelector('#year').textContent = new Date().getFullYear();
 document.querySelector('#lastupdated').textContent = `Last Modification: ${document.lastModified}`;
+
+
+/*Join Us Banner */
+const day = new Date();
+const today = day.getDay();
+
+if (today < 1 || today > 2) {
+    const banner = document.querySelector('.meeting');
+    banner.remove();
+}  
