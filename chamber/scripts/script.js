205df8c26224aa28a985cebb2a70b8db7761c2e3
Natalia <nparedes517@yahoo.com>
2022-10-27 22:22:24 -0400

    last visit code was added

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -19,9 +19,28 @@ document.querySelector('#lastupdated').textContent = `Last Modification: ${docum
 
 /*Join Us Banner */
 const day = new Date();
+const day2 = new Date();
+
+
 const today = day.getDay();
 
 if (today < 1 || today > 2) {
     const banner = document.querySelector('.meeting');
     banner.remove();
 }  
+
+
+/* Last visit */
+
+const lastVisit = localStorage.getItem('date');
+localStorage.setItem('date', day);
+
+const lastVisitDay = Date.parse(lastVisit);
+let numberOfDays = (day - lastVisitDay)/86400000;
+
+if (!numberOfDays) {
+    document.querySelector("#lastVisit").textContent = 0;
+}
+else {
+    document.querySelector("#lastVisit").textContent = numberOfDays.toFixed(0);
+}
