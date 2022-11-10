d5fa3a3f37048ad79465351665b71dd37b4add5c
Natalia <nparedes517@yahoo.com>
2022-11-10 16:10:19 -0500

    directory for chamber

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -57,8 +57,74 @@ else {
     // document.querySelector("#lastVisit").textContent = number_mSdayS.toFixed(0);
 }
 
-
+/*** FORM ****/
 /*----- Hidden date and time for the join form ----- */
 
 const joinDateCurrent = document.querySelector(".joinDate");
 if (joinDateCurrent) { joinDateCurrent.textContent = day }
+
+
+/*** DIRECTORY *******/
+
+const displayDirectory = (dataDirectory) => {
+    dataDirectory.companies.forEach (
+        company => {
+        let card = document.createElement('section');
+        let name = document.createElement('h3')
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let URL = document.createElement('p');
+        let email = document.createElement('p');
+        
+        name.textContent = `${company.name}`;
+
+        logo.setAttribute('src', company.image);
+        logo.setAttribute('alt', `Photo of ${company.name}`);
+        logo.setAttribute('loading', 'lazy');
+       
+        address.textContent = `${company.address}`;
+        phoneNumber.textContent = `${company.phoneNumber}`;
+        URL.textContent = `${company.URL}`;
+        email.textContent = `${company.email}`;
+        
+        card.appendChild(name);
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(phoneNumber);
+        card.appendChild(URL);
+        card.appendChild(email);
+        document.querySelector('article.directory').appendChild(card);
+      }
+    )
+  }
+
+/*------ JSON application--------*/  
+async function getDirectory() {
+    const response = await fetch("./json/data.json");
+    if (response.ok) {
+      let data = await response.json();
+      console.log(data);
+      displayDirectory(data);
+    }
+  }
+  const dir = document.querySelector('.directory');
+  if (dir) {
+    getDirectory();
+  }
+
+/*------------Button Listeners ----------------*/
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+	display.classList.add("list");
+	display.classList.remove("grid");
+});
